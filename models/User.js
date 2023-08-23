be4b208081b9c69630032d37a96d75ddbc6fe956
Jay Liu <jayliu89@gmail.com>
2023-08-23 09:59:11 -0700

    updated git ignore file correctly removed node_modules. created thought and user models

--- models/User.js
+++ models/User.js
@@ -13,9 +13,4 @@ userSchema.virtual("friendCount").get(function() {
   return this.friends.length
 })
 
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: { type: String, require: true, minLength:1, maxLength: 280 },
-  createdAt: {type: Date, default: Date.now},
-  username: {type: String, required: true},
-  reactions: [{}]
-})
+module.exports = User
