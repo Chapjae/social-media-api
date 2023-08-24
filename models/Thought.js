29050c3a25b1b9112c95a6ab4d6d3926c02bf60f
Jay Liu <jayliu89@gmail.com>
2023-08-23 17:06:02 -0700

    changed schema layouts

--- models/Thought.js
+++ models/Thought.js
@@ -1,10 +1,27 @@
-const mongoose = require("mongoose");
+const {Schema, Types} = require("mongoose");
+const reactionSchema = require("./Reaction")
 
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: { type: String, require: true, minLength:1, maxLength: 280 },
-  createdAt: {type: Date, default: Date.now},
-  username: {type: String, required: true},
+const thoughtSchema = new Schema(
+  {
+  thoughtId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId
+  },
+  thoughtText: { 
+    type: String, 
+    require: true, 
+    minLength:1, 
+    maxLength: 280 
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now
+  },
+  username: {
+    type: String, 
+    required: true
+  },
   reactions: [{}]
 })
 
-module.exports = Thought
+module.exports = thoughtSchema
--- models/User.js
+++ models/User.js
@@ -1,9 +1,21 @@
-const mongoose = require("mongoose");
+const {Schema, model} = require("mongoose");
+const thoughtSchema = require("./Thought")
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trimmed: true},
-  email: {type: String, required: true, unique: true, match: "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b." },
-  thoughts: [{type: Schema.type.ObjectId, ref: "Thought"}],
+const userSchema = new Schema(
+  {
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    trimmed: true
+  },
+  email: {
+    type: String, 
+    required: true, 
+    unique: true, 
+    match: "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b." 
+  },
+  thoughts: [thoughtSchema],
   friends: [{type: Schema.type.ObjectId, ref: "User"}] 
 }, {
   toJSON: {virutals: true}
