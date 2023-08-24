dbc13dfb94679ffcd6e460e943565b362d5e0dfe
Jay Liu <jayliu89@gmail.com>
2023-08-23 20:46:49 -0700

    added user routes to userRoutes file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const {User} = require("../models");
+const { User } = require("../models");
 
 module.exports = {
   async getUser(req, res) {
@@ -12,8 +12,27 @@ module.exports = {
 
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.courseId })
+      const user = await User.findOne({ _id: req.params.courseId });
+
+      if(!user){
+        return res.status(404).json({ message: "No User with that ID" });
+      }
+      
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
     }
   }
 
+  // if there's time, add delete user and delete thought
 }
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser
+} = require ("../../controllers/userController");
+
+router.route("/").get(getUser).post(createUser)
+
+router.route("/:userId").get(getSingleUser)
